Fix stack overflow error with checkpoint

benchmark.py
@@ -1,69 +1,143 @@
-from ridge import direct, res_norm, BCD, CABCD
-from pyspark.sql import SparkSession
+from pyspark import SparkContext, SparkConf
 from pyspark.mllib.random import RandomRDDs
-import numpy as np
 
 import numpy as np
-if __name__ == "__main__":
-	print("test")
-	
-	M = 2000
-	N = 100
-	mu = 2
-	l = 0.5
-	S = 5
-	eps = 0.05
-	spark = SparkSession.builder \
-		  .master('local') \
-		  .appName("Linear Regression Model Benchmark") \
-		  .config("spark.executor.memory", "16gb") \
-		  .config("spark.executor.instances", "1") \
-		  .getOrCreate()
-	sc = spark.sparkContext
-	sc.setLogLevel("ERROR")
-	A_p = RandomRDDs.uniformVectorRDD(sc, M, N, numPartitions=1, seed=16)
-	b_p = RandomRDDs.uniformRDD(sc, M, numPartitions=1, seed=20)
+import argparse
+import os
+import requests
+import sys
+from datetime import datetime
 
-	A = A_p.collect()
-	b = b_p.collect()
-	x_direct = direct(A, b, M, N, l)
+from ridge import direct, res_norm, BCD, CABCD
 
-	print("Direct: ", x_direct)
-	print("Resnorm: ", res_norm(A, b, x_direct, l))
-	print("\n")
 
-	nodes_num = np.arange(31,1,-1)
-	for node_num in nodes_num:
-		print("node_num:", node_num)
-		spark = SparkSession.builder \
-		  .master('local') \
-		  .appName("Linear Regression Model Benchmark") \
-		  .config("spark.executor.memory", "1gb") \
-		  .config("spark.executor.instances", str(node_num)) \
-		  .getOrCreate()
 
-		sc = spark.sparkContext
+def benchmark_mu(sc, A_p, b_p, M, N, l, eps):
+	mus = np.linspace(1, N, num=min(N, 20), endpoint=True).astype(int)
 
-		sc.setLogLevel("ERROR")
+	output_file = open("benchmark_mu.csv", "w")
+	output_file.write("mu,execution,sequential,remote,iterations\n")
 
-		A_p = RandomRDDs.uniformVectorRDD(sc, M, N, numPartitions=int(node_num), seed=16)
-		b_p = RandomRDDs.uniformRDD(sc, M, numPartitions=int(node_num), seed=20)
+	for mu in mus:
+		print("mu:", mu)
 
-		A = A_p.collect()
-		b = b_p.collect()
-
-		x_BCD, metrics_BCD = BCD(A_p, b_p, M, N, l, mu, eps)
+		x_BCD, metrics_BCD = BCD(sc, A_p, b_p, M, N, l, mu, eps)
 		#print("BCD: ", x_BCD)
 		#print("Resnorm: ", res_norm(A, b, x_BCD, l))
-		print("BCD Execution Time: ", metrics_BCD['execution'])
-		#print("Residual Time: ", metrics_BCD['residual'])
+		print("Execution Time: ", metrics_BCD['execution'])
+		print("Sequential Time: ", metrics_BCD['sequential'])
+		print("Remote Time: ", metrics_BCD['remote'])
 		print("Iterations: ", metrics_BCD['iterations'])
-		x_CABCD, metrics_CABCD = CABCD(A_p, b_p, M, N, l, mu, S, eps)
-		#print("CABCD: ", x_CABCD)
-		#print("Resnorm: ", res_norm(A, b, x_CABCD, l))
-		print("CABCD: Execution Time: ", metrics_CABCD['execution'])
-		#print("Residual Time: ", metrics_CABCD['residual'])
-		print("Iterations: ", metrics_CABCD['iterations'])
-		print("\n")
 
+		output_file.write(','.join([
+			str(mu), 
+			str(metrics_BCD['execution']), 
+			str(metrics_BCD['sequential']), 
+			str(metrics_BCD['remote']),
+			str(metrics_BCD['iterations'])
+		]))
+		output_file.write('\n')
+
+	output_file.close()
+
+
+
+
+
+def benchmark_S(sc, A_p, b_p, M, N, l, eps):
+	Ss = [1,2,3,5,10,15,20,30]
+
+	output_file = open("benchmark_S.csv", "w")
+	output_file.write("S,mu,execution,sequential,remote,iterations\n")
+
+	for S in Ss:
+		print("S:", S)
+
+		mus = np.linspace(1, N, num=min(N, 10), endpoint=True).astype(int)
+		for mu in mus:
+			print("mu:", mu)
+
+			x_CABCD, metrics_CABCD = CABCD(sc, A_p, b_p, M, N, l, mu, S, eps)
+			#print("CABCD: ", x_CABCD)
+			#print("Resnorm: ", res_norm(A, b, x_CABCD, l))
+			print("Execution Time: ", metrics_CABCD['execution'])
+			print("Sequential Time: ", metrics_CABCD['sequential'])
+			print("Remote Time: ", metrics_CABCD['remote'])
+			print("Iterations: ", metrics_CABCD['iterations'])
+			print("\n")
+
+			output_file.write(','.join([
+				str(S), 
+				str(mu), 
+				str(metrics_BCD['execution']), 
+				str(metrics_BCD['sequential']), 
+				str(metrics_BCD['remote']),
+				str(metrics_BCD['iterations'])
+			]))
+			output_file.write('\n')
+
+	output_file.close()
+
+
+
+def communication_time(sc):
+	appId = sc.applicationId
+	url = sc.uiWebUrl + '/api/v1/applications/' + appId
+	resp = requests.get(url=url + '/jobs')
+	jobs = resp.json()
+
+	comm_time = 0
+	for j in jobs:
+		t1 = datetime.strptime(j['submissionTime'], '%Y-%m-%dT%H:%M:%S.%f%Z')
+		t2 = datetime.strptime(j['completionTime'], '%Y-%m-%dT%H:%M:%S.%f%Z')
+		dt = t2-t1
+		comm_time += dt.microseconds/1000
+
+	return comm_time
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
 	
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-n", "--num-partitions", 	required=False, 	default=1, 		help="number of partitions")
+	ap.add_argument("-e", "--num-executors", 	required=False, 	default=1, 		help="number of executor instances")
+	ap.add_argument("-M", "--matrix-rows", 		required=False, 	default=10, 	help="number of rows in matrix")
+	ap.add_argument("-N", "--matrix-cols", 		required=False, 	default=10, 	help="number of cols in matrix")	
+	ap.add_argument("-u", "--block-size", 		required=False, 	default=1, 		help="block size")
+	ap.add_argument("-S", "--group-iterations", required=False, 	default=1, 		help="number of grouped iterations")
+	ap.add_argument("-p", "--precision", 		required=False, 	default=0.1,	help="precision")
+	args = vars(ap.parse_args())
+
+
+	numPartitions = int(args["num_partitions"])
+	numExecutors = int(args["num_executors"])
+	M = int(args["matrix_rows"])
+	N = int(args["matrix_cols"])
+	mu = int(args["block_size"])
+	S = int(args["group_iterations"])
+	eps = float(args["precision"])
+		
+	
+
+
+
+	conf = SparkConf().setAll([('spark.executor.cores', '1'), ('spark.executor.instances', str(numExecutors))])
+	sc = SparkContext(conf=conf)
+	sc.setCheckpointDir(os.getcwd())
+
+	A_p = RandomRDDs.uniformVectorRDD(sc, M, N, numPartitions=numPartitions, seed=1)
+	b_p = RandomRDDs.uniformRDD(sc, M, numPartitions=numPartitions, seed=2)
+	
+
+	l = 0.5
+
+	benchmark_mu(sc, A_p, b_p, M, N, l, eps)
+	benchmark_S(sc, A_p, b_p, M, N, l, eps)

ridge.py
@@ -2,6 +2,7 @@ from pyspark import SparkContext, SparkConf
 from pyspark.mllib.random import RandomRDDs
 import argparse
 import numpy as np
+import os
 import sys
 import time
 
@@ -16,41 +17,54 @@ def direct(A, b, M, N, l):
 	return np.dot(ATA, ATb)
 
 
-def BCD(A, b, M, N, l, mu, eps, max_iters=1000):
+def BCD(sc, A, b, M, N, l, mu, eps, max_iters=500):
 	count = 0
 
-	A.cache()
-	b.cache()
+	A = A.cache()
+	b = b.cache()
 
 	x = np.zeros(N)
-	z = b.map(lambda e: -e)
+	z = b.map(lambda e: -e).cache()
 
-	metrics = {
-		'iterations': 0,
-		'execution': 0,
-		'residual': 0
-	}
+	metrics = {}
 
-	execution_start = time.time()
+	time_execution = 0
+	time_sequential = 0
+	time_remote = 0
 
-	while count < max_iters:
+	AA = A.map(lambda row: (0, N, mu, row)).cache()
 
-		# Select indices
-		idx = np.arange(N)
-		np.random.shuffle(idx)
-		idx = idx[:mu]
+	while True:
+
+		iteration_total = 0
+		iteration_remote = 0
+		iteration_sequential = 0
+
+		iteration_start = time.time()
+
+
+		iteration_remote_start = time.time()
 
 		# Sub matrix of A
-		Ah = A.map(lambda row: row[idx])
+		Ah = AA.map(lambda e: e[3][np.random.RandomState(seed=count).permutation(e[1])[:e[2]]]).cache()
+		# AA = AA.map(lambda e: (e[0]+1, e[1], e[2], e[3]))
 
 		# Ah T * Ah
-		dotAh = Ah.map(lambda row: np.outer(row, row))
-		rh = Ah.zip(z).map(lambda t: t[0]*t[1])
+		rDotAh = Ah.map(lambda row: np.outer(row, row))
+		rRh = Ah.zip(z).map(lambda t: t[0]*t[1])
 
-		aux = dotAh.zip(rh).map(lambda t: np.concatenate((t[0],np.reshape(t[1], (mu, 1))), axis=1)).sum()
+		# aux = dotAh.zip(rh).map(lambda t: np.concatenate((t[0],np.reshape(t[1], (mu, 1))), axis=1)).sum()
+		dotAh, rh = rDotAh.zip(rRh).reduce(lambda t0, t1: (t0[0]+t1[0], t0[1]+t1[1]))
 
-		dotAh = l*np.eye(mu) + aux[:, 0:mu]
-		rh = -aux[:,-1]
+		iteration_remote += time.time() - iteration_remote_start
+		
+		iteration_sequential_start = time.time()
+
+		# Select indices
+		idx = np.random.RandomState(seed=count).permutation(N)[:mu]
+
+		dotAh += l*np.eye(mu)
+		rh = -rh
 
 
 		# (Ah T * Ah)^-1
@@ -64,8 +78,23 @@ def BCD(A, b, M, N, l, mu, eps, max_iters=1000):
 		for i in range(mu):
 			x[idx[i]] += dxh[i]
 
-		dz = Ah.map(lambda row: np.dot(row, dxh))
-		z = z.zip(dz).map(lambda t: t[0] + t[1])
+		iteration_sequential += time.time() - iteration_sequential_start
+
+		iteration_remote_start = time.time()
+
+		dxh_bcast = sc.broadcast(dxh)
+
+		dz = Ah.map(lambda row: np.dot(row, dxh_bcast.value))
+		z = z.zip(dz).map(lambda t: t[0] + t[1]).cache()
+
+		# Break lineage
+		if count % 50:
+			z.checkpoint()
+			z.count()
+
+		iteration_remote += time.time() - iteration_remote_start
+
+		iteration_total = time.time() - iteration_start
 
 		# Residue
 
@@ -85,55 +114,72 @@ def BCD(A, b, M, N, l, mu, eps, max_iters=1000):
 			r = ATb-ATAx-l*x
 
 			norm_r = np.linalg.norm(r)
-			metrics['residual'] += time.time() - residual_start
 			if norm_r < eps:
-				break
+				break	
 
+
+		time_sequential += iteration_sequential
+		time_remote += iteration_remote
+		time_execution += iteration_total
 		count += 1
 
-	metrics['execution'] += time.time() - execution_start
+		# print("Iteration ", count, " took ", iteration_total, "s (sequential: ", iteration_sequential, "s remote: ", iteration_remote, "s)")
+
+	metrics['execution'] = time_execution
+	metrics['sequential'] = time_sequential
+	metrics['remote'] = time_remote
 	metrics['iterations'] = count
 
 	return x, metrics
 
 
 
-def CABCD(A, b, M, N, l, mu, S, eps, max_iters=1000):
-	converged = False
+def CABCD(sc, A, b, M, N, l, mu, S, eps, max_iters=500):
 	count = 0
 
-	A.cache()
-	b.cache()
+	A = A.cache()
+	b = b.cache()
 
 	x = np.zeros(N)
-	alpha = b.map(lambda e: -e)
+	alpha = b.map(lambda e: -e).cache()
 
-	metrics = {
-		'iterations': 0,
-		'execution': 0,
-		'residual': 0
-	}
+	metrics = {}
 
-	execution_start = time.time()
+	time_execution = 0
+	time_sequential = 0
+	time_remote = 0
 
-	while not converged and count < max_iters:
+	AA = A.map(lambda row: (0, N, mu, S, row)).cache()
 
-		idx = np.zeros(S*mu)
-		for j in range(S):
-			# Select indices
-			coords = np.arange(N)
-			np.random.shuffle(coords)
-			idx[mu*j:mu*(j+1)] = coords[:mu]
+	while True:
 
-		idx = idx.astype(int)
+		iteration_total = 0
+		iteration_remote = 0
+		iteration_sequential = 0
+
+		iteration_start = time.time()
 
 
-		Y = A.map(lambda row: row[idx])
+		iteration_remote_start = time.time()
 
-		aux = Y.zip(alpha).map(lambda t: np.concatenate((np.outer(t[0], t[0]), np.reshape(t[0]*t[1], (S*mu, 1))), axis=1)).sum()
 
-		G = aux[:, :S*mu] + l*np.eye(S*mu)
-		Y_alpha = -aux[:, -1]
+		# Y = A.map(lambda row: row[idx])
+		Y = AA.map(lambda e: e[4][np.array([np.random.RandomState(seed=count+i).permutation(e[1])[:e[2]] for i in range(e[3])]).flatten()]).cache()
+		# AA = AA.map(lambda e: (e[0]+e[3], e[1], e[2], e[3], e[4])).cache()
+
+		# aux = Y.zip(alpha).map(lambda t: np.concatenate((np.outer(t[0], t[0]), np.reshape(t[0]*t[1], (S*mu, 1))), axis=1)).sum()
+		G = Y.map(lambda row: np.outer(row, row))
+		Y_alpha = Y.zip(alpha).map(lambda t: t[0]*t[1])
+		G, Y_alpha = G.zip(Y_alpha).reduce(lambda t0, t1: (t0[0]+t1[0], t0[1]+t1[1]))
+
+		iteration_remote += time.time() - iteration_remote_start
+		
+		iteration_sequential_start = time.time()
+
+		idx = np.array([np.random.RandomState(seed=count+i).permutation(N)[:mu] for i in range(S)]).flatten()
+
+		G += l*np.eye(S*mu)
+		Y_alpha = -Y_alpha
 
 
 		delta_x = np.zeros(S*mu)
@@ -152,13 +198,28 @@ def CABCD(A, b, M, N, l, mu, S, eps, max_iters=1000):
 		for i in range(S*mu):
 			x[idx[i]] = x[idx[i]] + delta_x[i]
 
+		iteration_sequential += time.time() - iteration_sequential_start
 
-		d_alpha = Y.map(lambda Y_row: np.dot(Y_row, delta_x))
-		alpha = alpha.zip(d_alpha).map(lambda t: t[0]+t[1])
+		iteration_remote_start = time.time()
+
+		delta_x_bcast = sc.broadcast(delta_x)
+
+
+		d_alpha = Y.map(lambda Y_row: np.dot(Y_row, delta_x_bcast.value))
+		alpha = alpha.zip(d_alpha).map(lambda t: t[0]+t[1]).cache()
+
+		# Break lineage
+		if count % 50:
+			alpha.checkpoint()
+			alpha.count()
+
+		iteration_remote += time.time() - iteration_remote_start
+
+		iteration_total = time.time() - iteration_start
 
 		# Residue
 
-		if count % 5 == 0:
+		if count/S % 5 == 0:
 
 			residual_start = time.time()
 
@@ -175,15 +236,19 @@ def CABCD(A, b, M, N, l, mu, S, eps, max_iters=1000):
 			r = ATb-ATAx-l*x
 
 			norm_r = np.linalg.norm(r)
-
-			metrics['residual'] += time.time() - residual_start
-
 			if norm_r < eps:
 				break
 
-		count += 1
+		time_sequential += iteration_sequential
+		time_remote += iteration_remote
+		time_execution += iteration_total
+		count += S
 
-	metrics['execution'] += time.time() - execution_start
+		# print("Iteration ", count, " took ", iteration_total, "s (sequential: ", iteration_sequential, "s remote: ", iteration_remote, "s)")
+
+	metrics['execution'] = time_execution
+	metrics['sequential'] = time_sequential
+	metrics['remote'] = time_remote
 	metrics['iterations'] = count
 
 	return x, metrics
@@ -203,12 +268,13 @@ if __name__ == "__main__":
 
 
 	ap = argparse.ArgumentParser()
-	ap.add_argument("-n", "--num-partitions", required=False, help="number of partitions")
-	ap.add_argument("-e", "--num-executors", required=False, default=1, help="number of executor instances")
-	ap.add_argument("-M", "--matrix-rows", required=False, default=10, help="number of rows in matrix")
-	ap.add_argument("-N", "--matrix-cols", required=False, default=10, help="number of cols in matrix")	
-	ap.add_argument("-u", "--block-size", required=False, default=1, help="block size")
-	ap.add_argument("-S", "--group-iterations", required=False, default=1, help="number of grouped iterations")
+	ap.add_argument("-n", "--num-partitions", 	required=False, 	default=1, 		help="number of partitions")
+	ap.add_argument("-e", "--num-executors", 	required=False, 	default=1, 		help="number of executor instances")
+	ap.add_argument("-M", "--matrix-rows", 		required=False, 	default=10, 	help="number of rows in matrix")
+	ap.add_argument("-N", "--matrix-cols", 		required=False, 	default=10, 	help="number of cols in matrix")	
+	ap.add_argument("-u", "--block-size", 		required=False, 	default=1, 		help="block size")
+	ap.add_argument("-S", "--group-iterations", required=False, 	default=1, 		help="number of grouped iterations")
+	ap.add_argument("-p", "--precision", 		required=False, 	default=0.1,	help="precision")
 	args = vars(ap.parse_args())
 
 
@@ -222,6 +288,7 @@ if __name__ == "__main__":
 
 	conf = SparkConf().setAll([('spark.executor.cores', '1'), ('spark.executor.instances', str(numExecutors))])
 	sc = SparkContext(conf=conf)
+	sc.setCheckpointDir(os.getcwd())
 
 
 	A_p = RandomRDDs.uniformVectorRDD(sc, M, N, numPartitions=numPartitions, seed=1)
@@ -240,21 +307,23 @@ if __name__ == "__main__":
 	print("Resnorm: ", res_norm(A, b, x_direct, l))
 	print("\n")
 
-	x_BCD, metrics_BCD = BCD(A_p, b_p, M, N, l, mu, eps)
+	x_BCD, metrics_BCD = BCD(sc, A_p, b_p, M, N, l, mu, eps)
 	print("BCD")
 	print(x_BCD)
 	print("Resnorm: ", res_norm(A, b, x_BCD, l))
 	print("Execution Time: ", metrics_BCD['execution'])
-	print("Residual Time: ", metrics_BCD['residual'])
+	print("Sequential Time: ", metrics_BCD['sequential'])
+	print("Remote Time: ", metrics_BCD['remote'])
 	print("Iterations: ", metrics_BCD['iterations'])
 	print("\n")
 
-	x_CABCD, metrics_CABCD = CABCD(A_p, b_p, M, N, l, mu, S, eps)
+	x_CABCD, metrics_CABCD = CABCD(sc, A_p, b_p, M, N, l, mu, S, eps)
 	print("CABCD")
 	print(x_CABCD)
 	print("Resnorm: ", res_norm(A, b, x_CABCD, l))
 	print("Execution Time: ", metrics_CABCD['execution'])
-	print("Residual Time: ", metrics_CABCD['residual'])
+	print("Sequential Time: ", metrics_CABCD['sequential'])
+	print("Remote Time: ", metrics_CABCD['remote'])
 	print("Iterations: ", metrics_CABCD['iterations'])
 	print("\n")
 