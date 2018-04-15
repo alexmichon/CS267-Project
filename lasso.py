7bcff2ad0327b71c0cf7f0724e6870e739680907
Alex Michon <alexmichon@hotmail.fr>
2018-04-14 19:25:35 -0700

    Lasso: CABCD

--- lasso.py
+++ lasso.py
@@ -10,7 +10,7 @@ import time
 
 
 
-def BCD(sc, A, b, M, N, l, mu, eps, max_iters=10):
+def BCD(sc, A, b, M, N, l, mu, eps, max_iters=20):
 	count = 0
 
 	A = A.cache()
@@ -98,7 +98,7 @@ def BCD(sc, A, b, M, N, l, mu, eps, max_iters=10):
 
 		iteration_total = time.time() - iteration_start
 
-		Residue
+		# Residue
 
 		# if count % 5 == 0:
 		# 	residual_start = time.time()
@@ -133,6 +133,164 @@ def BCD(sc, A, b, M, N, l, mu, eps, max_iters=10):
 	metrics['iterations'] = count
 
 	return theta*theta*y+z, metrics
+
+
+
+
+
+
+def CABCD(sc, A, b, M, N, l, mu, S, eps, max_iters=20):
+	count = 0
+
+	A = A.cache()
+	b = b.cache()
+
+	y = np.zeros(N)
+	yt = A.map(lambda row: np.dot(row, y)).cache()
+	yt.collect()
+
+	z = np.zeros(N)
+	zt = A.zip(b).map(lambda t: np.dot(t[0], z)-t[1]).cache()
+	zt.collect()
+
+	metrics = {}
+
+	time_execution = 0
+	time_sequential = 0
+	time_remote = 0
+
+	AA = A.map(lambda row: (0, N, mu, S, row)).cache()
+
+	thetas = np.zeros(S+1)
+	thetas[0] = float(mu)/N
+	q = int(N/mu)
+
+	while count < max_iters:
+
+		iteration_total = 0
+		iteration_remote = 0
+		iteration_sequential = 0
+
+		iteration_start = time.time()
+
+
+		iteration_remote_start = time.time()
+
+		for j in range(1, S+1):
+			prec = thetas[j-1]
+			thetas[j] = 0.5*(math.sqrt(pow(prec,4)+4*pow(prec,2))-pow(prec,2))
+
+
+		# Y = A.map(lambda row: row[idx])
+		Y = AA.map(lambda e: e[4][np.array([np.random.RandomState(seed=count+i).permutation(e[1])[:e[2]] for i in range(e[3])]).flatten()]).cache()
+		# AA = AA.map(lambda e: (e[0]+e[3], e[1], e[2], e[3], e[4])).cache()
+
+		# aux = Y.zip(alpha).map(lambda t: np.concatenate((np.outer(t[0], t[0]), np.reshape(t[0]*t[1], (S*mu, 1))), axis=1)).sum()
+		G = Y.map(lambda row: np.outer(row, row))
+		Yyt = Y.zip(yt).map(lambda t: t[0]*t[1])
+		Yzt = Y.zip(zt).map(lambda t: t[0]*t[1])
+		((G, Yyt), Yzt) = G.zip(Yyt).zip(Yzt).reduce(lambda t0, t1: ((t0[0][0]+t1[0][0], t0[0][1]+t1[0][1]), t0[1]+t1[1]))
+
+		iteration_remote += time.time() - iteration_remote_start
+		
+		iteration_sequential_start = time.time()
+
+		idx = np.array([np.random.RandomState(seed=count+i).permutation(N)[:mu] for i in range(S)]).flatten()
+
+
+		dz = np.zeros(S*mu)
+		for j in range(S):
+			Mj = G[mu*j:mu*(j+1), mu*j:mu*(j+1)]
+
+			r = thetas[j]*thetas[j]*Yyt[mu*j:mu*(j+1)]+Yzt[mu*j:mu*(j+1)]
+			for t in range(j):
+				r -= (thetas[j]*thetas[j]*(1-q*thetas[j])/thetas[t]/thetas[t]-1) * np.dot(G[mu*j:mu*(j+1), mu*t:mu*(t+1)], dz[mu*t:mu*(t+1)])
+			
+			v = max(np.linalg.eigvals(Mj))
+			n = 1/(q*thetas[j]*v)
+			
+			g = z.copy()
+			for t in range(j):
+				g[idx[mu*t:mu*(t+1)]] += dz[mu*t:mu*(t+1)]
+
+			g = g[idx[mu*j:mu*(j+1)]]-n*r
+
+			dz[mu*j:mu*(j+1)] = np.sign(g)*np.array([max(abs(e)-l*n, 0) for e in g]) - z[idx[mu*j:mu*(j+1)]]
+			dz2 = np.zeros(N)
+			for t in range(j):
+				dz2[idx[mu*t:mu*(t+1)]] += dz[mu*t:mu*(t+1)]
+			dz[mu*j:mu*(j+1)] -= dz2[idx[mu*j:mu*(j+1)]]
+
+		for j in range(S):
+			z[idx[mu*j:mu*(j+1)]] += dz[mu*j:mu*(j+1)]
+			y[idx[mu*j:mu*(j+1)]] -= (1-q*thetas[j])/thetas[j]/thetas[j]*dz[mu*j:mu*(j+1)]
+
+		iteration_sequential += time.time() - iteration_sequential_start
+
+		iteration_remote_start = time.time()
+
+		dz_bcast = sc.broadcast(dz)
+
+
+		dzt = Y.map(lambda row: np.dot(row, dz_bcast.value)).cache()
+		zt = zt.zip(dzt).map(lambda t: t[0] + t[1]).cache()
+		print(zt.collect())
+		dyt = Y.map(lambda row: sum([(1-q*thetas[j])/thetas[j]/thetas[j]*np.dot(row[mu*j:mu*(j+1)], dz_bcast.value[mu*j:mu*(j+1)]) for j in range(S)])).cache()
+		yt = yt.zip(dyt).map(lambda t: t[0] - t[1]).cache()
+		print(yt.collect())
+
+		thetas[0] = thetas[S]
+
+
+		# Break lineage
+		if count % 50:
+			zt.checkpoint()
+			zt.count()
+			yt.checkpoint()
+			yt.count()
+
+		iteration_remote += time.time() - iteration_remote_start
+
+		iteration_total = time.time() - iteration_start
+
+		# Residue
+
+		# if count/S % 5 == 0:
+
+		# 	residual_start = time.time()
+
+		# 	# A*x
+		# 	Ax = A.map(lambda row: np.dot(x, row))
+
+		# 	# AT*A*x
+		# 	ATAx = A.zip(Ax).map(lambda t: t[0] * t[1]).sum()
+
+		# 	# AT*b
+		# 	ATb = A.zip(b).map(lambda t: t[0]*t[1]).sum()
+
+		# 	# r = AT*b - AT*A*x
+		# 	r = ATb-ATAx-l*x
+
+		# 	norm_r = np.linalg.norm(r)
+		# 	if norm_r < eps:
+		# 		break
+
+		time_sequential += iteration_sequential
+		time_remote += iteration_remote
+		time_execution += iteration_total
+		count += S
+
+		# print("Iteration ", count, " took ", iteration_total, "s (sequential: ", iteration_sequential, "s remote: ", iteration_remote, "s)")
+
+	metrics['execution'] = time_execution
+	metrics['sequential'] = time_sequential
+	metrics['remote'] = time_remote
+	metrics['iterations'] = count
+
+	return thetas[-1]*thetas[-1]*y+z, metrics
+
+
+
 
 
 
@@ -189,5 +347,15 @@ if __name__ == "__main__":
 	print("Sequential Time: ", metrics_BCD['sequential'])
 	print("Remote Time: ", metrics_BCD['remote'])
 	print("Iterations: ", metrics_BCD['iterations'])
+	print("\n")
+
+	x_CABCD, metrics_CABCD = CABCD(sc, A_p, b_p, M, N, l, mu, S, eps)
+	print("CABCD")
+	print(x_CABCD)
+	print("Resnorm: ", res_norm(A, b, x_CABCD, l))
+	print("Execution Time: ", metrics_CABCD['execution'])
+	print("Sequential Time: ", metrics_CABCD['sequential'])
+	print("Remote Time: ", metrics_CABCD['remote'])
+	print("Iterations: ", metrics_CABCD['iterations'])
 	print("\n")
 
