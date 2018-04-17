849ad7f3f4e3590b4c87cc4c278f170af0a2d5a9
Alex Michon <alexmichon@hotmail.fr>
2018-04-16 17:39:50 -0700

    Fix ridge dual

--- ridge_dual.py
+++ ridge_dual.py
@@ -1,7 +1,11 @@
-from pyspark.sql import SparkSession
+from pyspark import SparkContext, SparkConf
 from pyspark.mllib.random import RandomRDDs
+import argparse
 import numpy as np
+import os
+import sys
 import time
+
 
 
 
@@ -15,37 +19,48 @@ def direct(A, b, M, N, l):
 def BDCD(sc, A, b, M, N, l, mu, eps, max_iters=50):
 	count = 0
 
-	A.cache()
+	A = A.cache()
 
-	x = sc.parallelize(np.zeros(N))
+	x = sc.parallelize(np.zeros(N)).cache()
 	alpha = np.zeros(M)
 
-	metrics = {
-		'iterations': 0,
-		'execution': 0,
-		'residual': 0
-	}
+	metrics = {}
 
-	execution_start = time.time()
+	time_execution = 0
+	time_sequential = 0
+	time_remote = 0
 
 	while count < max_iters:
 
-		# Select indices
-		idx = np.arange(M)
-		np.random.shuffle(idx)
-		idx = idx[:mu]
+		iteration_total = 0
+		iteration_remote = 0
+		iteration_sequential = 0
+
+		iteration_start = time.time()
+
+
+		iteration_remote_start = time.time()
 
 		# Sub matrix of A
-		Ah = A.map(lambda col: col[idx])
+		Ah = A.map(lambda col: col[np.random.RandomState(seed=count).permutation(M)[:mu]]).cache()
 
 		# Ah T * Ah
 		dotAh = Ah.map(lambda col: np.outer(col, col))
 		rh = Ah.zip(x).map(lambda t: t[0]*t[1])
 
-		aux = dotAh.zip(rh).map(lambda t: np.concatenate((t[0],np.reshape(t[1], (mu, 1))), axis=1)).sum()
+		# aux = dotAh.zip(rh).map(lambda t: np.concatenate((t[0],np.reshape(t[1], (mu, 1))), axis=1)).sum()
+		dotAh, rh = dotAh.zip(rh).reduce(lambda t0, t1: (t0[0]+t1[0], t0[1]+t1[1]))
 
-		dotAh = np.eye(mu) + (1/l)*aux[:, :mu]
-		rh = -aux[:,-1] + alpha[idx] - b[idx]
+		iteration_remote += time.time() - iteration_remote_start
+		
+		iteration_sequential_start = time.time()
+
+		# Select indices
+		idx = np.random.RandomState(seed=count).permutation(M)[:mu]
+
+
+		dotAh = (1/l)*dotAh + np.eye(mu)
+		rh = -rh + alpha[idx] - b[idx]
 
 
 		# (Ah T * Ah)^-1
@@ -60,8 +75,24 @@ def BDCD(sc, A, b, M, N, l, mu, eps, max_iters=50):
 		# 	alpha[idx[i]] += dalphah[i]
 		alpha[idx] += dalphah
 
-		dx = Ah.map(lambda col: np.dot(col, dalphah))
+		iteration_sequential += time.time() - iteration_sequential_start
+
+		iteration_remote_start = time.time()
+
+		dalphah_bcast = sc.broadcast(dalphah)
+
+		dx = Ah.map(lambda col: np.dot(col, dalphah_bcast.value))
 		x = x.zip(dx).map(lambda t: t[0] - (1/l)*t[1])
+
+		# Break lineage
+		if count % 50:
+			x.checkpoint()
+			x.count()
+
+
+		iteration_remote += time.time() - iteration_remote_start
+
+		iteration_total = time.time() - iteration_start
 
 		# Residue
 
@@ -78,13 +109,17 @@ def BDCD(sc, A, b, M, N, l, mu, eps, max_iters=50):
 			r = (1/l)*AATalpha+alpha-b
 
 			norm_r = np.linalg.norm(r)
-			metrics['residual'] += time.time() - residual_start
 			if norm_r < eps:
 				break
 
+		time_sequential += iteration_sequential
+		time_remote += iteration_remote
+		time_execution += iteration_total
 		count += 1
 
-	metrics['execution'] += time.time() - execution_start
+	metrics['execution'] = time_execution
+	metrics['sequential'] = time_sequential
+	metrics['remote'] = time_remote
 	metrics['iterations'] = count
 
 	return alpha, x, metrics
@@ -97,21 +132,30 @@ def BDCD(sc, A, b, M, N, l, mu, eps, max_iters=50):
 def CABDCD(sc, A, b, M, N, l, mu, S, eps, max_iters=1000):
 	count = 0
 
-	A.cache()
+	A = A.cache()
 	
 
-	x = sc.parallelize(np.zeros(N))
+	x = sc.parallelize(np.zeros(N)).cache()
 	alpha = np.zeros(M)
 
-	metrics = {
-		'iterations': 0,
-		'execution': 0,
-		'residual': 0
-	}
+	metrics = {}
 
-	execution_start = time.time()
+	time_execution = 0
+	time_sequential = 0
+	time_remote = 0
 
 	while count < max_iters:
+
+
+		iteration_total = 0
+		iteration_remote = 0
+		iteration_sequential = 0
+
+		iteration_start = time.time()
+
+
+		iteration_remote_start = time.time()
+
 
 		idx = np.zeros(S*mu)
 		for j in range(S):
@@ -123,13 +167,24 @@ def CABDCD(sc, A, b, M, N, l, mu, S, eps, max_iters=1000):
 		idx = idx.astype(int)
 
 
-		YT = A.map(lambda col: col[idx])
+		YT = A.map(lambda col: col[np.array([np.random.RandomState(seed=count+i).permutation(M)[:mu] for i in range(S)]).flatten()]).cache()
 
-		aux = YT.zip(x).map(lambda t: np.concatenate((np.outer(t[0], t[0]), np.reshape(t[0]*t[1], (S*mu, 1))), axis=1)).sum()
+
+		G = YT.map(lambda col: np.outer(col, col))
+		Y_x = YT.zip(x).map(lambda t: t[0]*t[1])
+		G, Y_x = G.zip(Y_x).reduce(lambda t0, t1: (t0[0]+t1[0], t0[1]+t1[1]))
+
+		iteration_remote += time.time() - iteration_remote_start
+		
+		iteration_sequential_start = time.time()
+
+		# aux = YT.zip(x).map(lambda t: np.concatenate((np.outer(t[0], t[0]), np.reshape(t[0]*t[1], (S*mu, 1))), axis=1)).sum()
+
+
+		idx = np.array([np.random.RandomState(seed=count+i).permutation(M)[:mu] for i in range(S)]).flatten()
 
 		# G = (1/l) * YT.map(lambda col: np.outer(col, col)).sum() + np.eye(S*mu)
-		G = (1/l)*aux[:, :S*mu] + np.eye(S*mu)
-		Y_x = aux[:, -1]
+		G = (1/l)*G + np.eye(S*mu)
 
 
 		delta_a = np.zeros(S*mu)
@@ -149,8 +204,24 @@ def CABDCD(sc, A, b, M, N, l, mu, S, eps, max_iters=1000):
 			alpha[idx[i]] = alpha[idx[i]] + delta_a[i]
 
 
-		d_x = YT.map(lambda Y_row: np.dot(Y_row, delta_a))
+		iteration_sequential += time.time() - iteration_sequential_start
+
+		iteration_remote_start = time.time()
+
+		delta_a_bcast = sc.broadcast(delta_a)
+
+
+		d_x = YT.map(lambda Y_row: np.dot(Y_row, delta_a_bcast.value))
 		x = x.zip(d_x).map(lambda t: t[0]-(1/l)*t[1])
+
+		# Break lineage
+		if count % 50:
+			x.checkpoint()
+			x.count()
+
+		iteration_remote += time.time() - iteration_remote_start
+
+		iteration_total = time.time() - iteration_start
 
 		# Residue
 
@@ -167,13 +238,17 @@ def CABDCD(sc, A, b, M, N, l, mu, S, eps, max_iters=1000):
 			r = (1/l)*AATalpha+alpha-b
 
 			norm_r = np.linalg.norm(r)
-			metrics['residual'] += time.time() - residual_start
 			if norm_r < eps:
 				break
 
-		count += 1
+		time_sequential += iteration_sequential
+		time_remote += iteration_remote
+		time_execution += iteration_total
+		count += S
 
-	metrics['execution'] += time.time() - execution_start
+	metrics['execution'] = time_execution
+	metrics['sequential'] = time_sequential
+	metrics['remote'] = time_remote
 	metrics['iterations'] = count
 
 	return alpha, x, metrics
@@ -193,17 +268,29 @@ def res_norm(A, b, y, l):
 
 if __name__ == "__main__":
 
-	spark = SparkSession.builder \
-	  .master('local') \
-	  .appName("Linear Regression Model") \
-	  .config("spark.executor.memory", "1gb") \
-	  .getOrCreate()
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-n", "--num-partitions", 	required=False, 	default=1, 		help="number of partitions")
+	ap.add_argument("-e", "--num-executors", 	required=False, 	default=1, 		help="number of executor instances")
+	ap.add_argument("-M", "--matrix-rows", 		required=False, 	default=10, 	help="number of rows in matrix")
+	ap.add_argument("-N", "--matrix-cols", 		required=False, 	default=10, 	help="number of cols in matrix")	
+	ap.add_argument("-u", "--block-size", 		required=False, 	default=1, 		help="block size")
+	ap.add_argument("-S", "--group-iterations", required=False, 	default=1, 		help="number of grouped iterations")
+	ap.add_argument("-p", "--precision", 		required=False, 	default=0.1,	help="precision")
+	args = vars(ap.parse_args())
 
-	sc = spark.sparkContext
-	sc.setLogLevel("ERROR")
 
-	M = 5
-	N = 10
+	numPartitions = int(args["num_partitions"])
+	numExecutors = int(args["num_executors"])
+	M = int(args["matrix_rows"])
+	N = int(args["matrix_cols"])
+	mu = int(args["block_size"])
+	S = int(args["group_iterations"])
+	eps = float(args["precision"])
+
+
+	conf = SparkConf().setAll([('spark.executor.cores', '1'), ('spark.executor.instances', str(numExecutors))])
+	sc = SparkContext(conf=conf)
+	sc.setCheckpointDir(os.getcwd())
 
 
 	A_p = RandomRDDs.uniformVectorRDD(sc, N, M, seed=1)
@@ -212,13 +299,7 @@ if __name__ == "__main__":
 	A = np.array(A_p.collect()).T
 	b = np.array(b_p.collect())
 
-
-	mu = 2
 	l = 0.5
-
-	S = 5
-
-	eps = 0.1
 
 	y_direct = direct(A, b, M, N, l)
 	print("Direct")
@@ -231,7 +312,8 @@ if __name__ == "__main__":
 	print(y_BDCD)
 	print("Resnorm: ", res_norm(A, b, y_BDCD, l))
 	print("Execution Time: ", metrics_BDCD['execution'])
-	print("Residual Time: ", metrics_BDCD['residual'])
+	print("Sequential Time: ", metrics_BDCD['sequential'])
+	print("Remote Time: ", metrics_BDCD['remote'])
 	print("Iterations: ", metrics_BDCD['iterations'])
 	print("\n")
 
@@ -240,6 +322,7 @@ if __name__ == "__main__":
 	print(y_CABDCD)
 	print("Resnorm: ", res_norm(A, b, y_CABDCD, l))
 	print("Execution Time: ", metrics_CABDCD['execution'])
-	print("Residual Time: ", metrics_CABDCD['residual'])
+	print("Sequential Time: ", metrics_CABDCD['sequential'])
+	print("Remote Time: ", metrics_CABDCD['remote'])
 	print("Iterations: ", metrics_CABDCD['iterations'])
 	print("\n")
